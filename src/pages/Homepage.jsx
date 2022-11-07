7b71a85cfe1b456907692d76178a0b59d0175ba3
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-08 00:05:08 +0100

    .......

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
 import { GiLoveMystery } from "react-icons/gi";
-import { Flex, Button, Box, Text, useToast, Center } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Box,
+  Text,
+  useToast,
+  Center,
+  Img,
+} from "@chakra-ui/react";
 import SubmitProject from "../components/SubmitProject";
 import ProjectCard from "../components/ProjectCard";
-
+import logo from "../assets/logo.png";
 const Homepage = () => {
   const [showPopup, setShowPopup] = useState(false);
   const closePopup = () => {
@@ -93,12 +101,11 @@ const Homepage = () => {
         closePopup={closePopup}
         addNewProject={addNewProject}
       />
-      <Flex justifyContent={"space-between"} mx={"50px"} pt={"20px"}>
-        <Text fontSize={"30px"} fontFamily={"Roboto"}>
-          MadeWithChakra
-        </Text>
-        <Box>
-          <Button
+      <Box h={"100vh"}>
+        <Flex justifyContent={"space-between"}>
+          <Img w={"300px"} src={logo} />
+          <Box>
+            {/* <Button
             color={"white"}
             leftIcon={<GiLoveMystery />}
             bg={"teal"}
@@ -107,46 +114,48 @@ const Homepage = () => {
             }}
           >
             Sponsor
+          </Button> */}
+          </Box>
+        </Flex>
+        <Box
+          direction={"column"}
+          justifyContent={"center"}
+          textAlign={"center"}
+          pt={"50px"}
+          alignItems={"center"}
+          w={["80%", "60%"]}
+          m={"auto"}
+        >
+          <Text fontSize={"60px"} fontFamily={"Roboto"}>
+            Showcase your Projects made with Chakra-ui
+          </Text>
+          <Text mt={["15px", "20px"]}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad illum
+            quia nam possimus tenetur autem est nostrum doloremque maiores ipsum
+            quaerat praesentium architecto voluptas quisquam, voluptatem fuga
+            numquam eaque voluptatibus.
+          </Text>
+          <Button
+            borderRadius={"10px"}
+            mt={"20px"}
+            color={"white"}
+            p={"30px"}
+            bg={"teal"}
+            _hover={{
+              bg: "#4cbf87",
+            }}
+            onClick={() => setShowPopup(true)}
+          >
+            Submit Projects
           </Button>
         </Box>
-      </Flex>
-      <Flex
-        direction={"column"}
-        justifyContent={"center"}
-        textAlign={"center"}
-        pt={"100px"}
-        alignItems={"center"}
-        w={["80%", "60%"]}
-        m={"auto"}
-      >
+      </Box>
+      <Center>
         <Text fontSize={"60px"} fontFamily={"Roboto"}>
-          Showcase your Projects made with Chakra-ui
+          Project
         </Text>
-        <Text mt={["15px", "20px"]}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad illum
-          quia nam possimus tenetur autem est nostrum doloremque maiores ipsum
-          quaerat praesentium architecto voluptas quisquam, voluptatem fuga
-          numquam eaque voluptatibus.
-        </Text>
-        <Button
-          borderRadius={"10px"}
-          mt={"20px"}
-          color={"white"}
-          p={"30px"}
-          bg={"teal"}
-          _hover={{
-            bg: "#4cbf87",
-          }}
-          onClick={() => setShowPopup(true)}
-        >
-          Submit Projects
-        </Button>
-        <Center>
-          <Text mt={"50px"} fontSize={"60px"} fontFamily={"Roboto"}>
-            Project
-          </Text>
-        </Center>
-      </Flex>
+      </Center>
+
       <Flex
         pb={"50px"}
         mt={"10px"}
