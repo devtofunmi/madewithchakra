6349966e5724b089ed51ce903b32f49646a17d5d
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-07 10:05:50 +0100

    ..

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const ProjectCard = ({ projectName, url, twitterHandle, description }) => {
   return (
-    <Box width={["90%", "45%", "25%"]} borderRadius={"10px"} bg={"#202225"}>
+    <Box width={["90%", "45%", "30%"]} borderRadius={"10px"} bg={"#202225"}>
       <Box>
         <iframe width={"100%"} src={url}></iframe>
       </Box>
-      <Box p={"17px"}>
+      <Box p={"10px"}>
         <Text fs={"30px"}>{projectName}</Text>
         <Text>{twitterHandle}</Text>
         <Text>{description}</Text>
