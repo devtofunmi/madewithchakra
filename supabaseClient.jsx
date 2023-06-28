2eb937b2f78711c5e9fe1fb6d03e803178becfcc
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-28 06:19:42 +0100

    ..

--- supabaseClient.jsx
+++ supabaseClient.jsx
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRodGJmdGx0c2dwY3d5aGp2a2lyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzkwNTgyMCwiZXhwIjoxOTgzNDgxODIwfQ.pNLEa4B-Rm8xviKJ2sCCHmF6994SHk_oYbp0xLLW5RE";
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
